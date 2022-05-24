8011bc96815dc8808ea4bbb9cc403cf49a9390c3
Matheus Calegaro <hello@matheus.me>
2022-05-24 01:07:01 -0300

    feat(server): configure http application server alongside mqtt microsservice

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  await app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.MQTT,
     options: {
       url: `mqtt://${process.env.MQTT_BROKER_HOST || 'localhost'}:${
@@ -13,7 +21,8 @@ async function bootstrap() {
     },
   });
 
-  app.listen();
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
 }
 
 bootstrap();
